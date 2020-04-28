f1ee931d270930591896aad68a3afa6fb8fe24f1
Mark Leung <mark@Marburys-MacBook-Pro.local>
2020-04-28 19:03:06 +0800

    auto

--- coronavirus.py
+++ coronavirus.py
@@ -16,7 +16,7 @@ class CV():
 		self.ratio = (self.new / (self.active + 1)).iloc[:, self.gap:]
 		self.new = self.new.iloc[:, self.gap:]
 
-	def getPlot(self, names, folder = None, width = 13):
+	def getPlot(self, names, write = None, width = 13):
 		f = plt.figure(figsize = (width, 6))
 		# If width < 13, print left graph only
 		if width >= 13:
@@ -34,25 +34,25 @@ class CV():
 			plt.ylim([1, 1e6])
 			plt.grid(True, 'both')
 		# Save or Show?
-		plt.savefig('%s/%s.png' % (folder, '-'.join(names))) if folder else plt.show()
+		plt.savefig('%s/%s.png' % (self.filename, '-'.join(names))) if write else plt.show()
 		plt.close(f)
 
 	def getUpdated(self):
-		old = pd.read_excel(self.filename).columns[-1]
+		old = pd.read_excel(self.filename+'.xlsx').columns[-1]
 		new = self.total.columns[-1]
 		print('old', old, 'new', new)
 		return new != old
 
 	def writeOut(self):
-		writer = pd.ExcelWriter(self.filename)
+		writer = pd.ExcelWriter(self.filename+'.xlsx')
 		for name in 'total', 'active', 'new', 'ratio':
 			eval('self.'+name).replace(0, float('nan')).to_excel(writer, name)
 		writer.save()
 
-	def plotTop(self, folder, top = 1000):
+	def plotTop(self, top = 1000):
 		lastColumn = self.total.columns[-1]
 		for country in tqdm.tqdm(self.total[self.total[lastColumn] > top].index):
-			self.getPlot([country], folder = folder)
+			self.getPlot([country], write = True)
 
 	def __init__(self, gap, filename, total, active = None):
 		self.filename = filename
@@ -70,18 +70,19 @@ def getWorld(used):
 	total = makeSource('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 	active = total - makeSource('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
 	# Call Class
-	return CV(7, 'time_series.xlsx', total, active, )
+	return CV(7, 'time_series_world', total, active, )
 
 def getUS():
 	total = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv').drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Country_Region', 'Lat', 'Long_', 'Combined_Key'], axis=1).groupby('Province_State').sum()
-	return CV(7, 'time_series_us.xlsx', total, )
+	return CV(7, 'time_series_us', total, )
 
 if __name__=='__main__':
 	world = getWorld(['Hong Kong', 'Macau', 'Hubei', 'Guangdong'])
 	us = getUS()
-	if world.getUpdated():
+	override = False
+	if world.getUpdated() or override:
 		world.writeOut()
-		world.plotTop('Graphs')
-	if us.getUpdated():
+		world.plotTop()
+	if us.getUpdated() or override:
 		us.writeOut()
-		us.plotTop('US')
+		us.plotTop()
